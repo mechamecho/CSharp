e63fbf18e6319950299188e50b16056f6d623880
Livepurposefully <nafissa_92@hotmail.com>
2017-11-15 17:21:27 -0500

    Learning Array Methods in C#

---
+++ ArrayMethods/ArrayMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 4, 7 };
+            //Properties
+
+            Console.WriteLine(arr.Length);
+            //Outputs 3
+            Console.WriteLine(arr.Rank);
+            //Outputs 1
+
+            //Methods
+            Console.WriteLine(arr.Max());
+            Console.WriteLine(arr.Min());
+            Console.WriteLine(arr.Sum());
+
+        }
+    }
+}
